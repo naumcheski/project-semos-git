f2f36ebd3c4c67abbccd572fb5745a1b4d8b0762
naumcheski <72261890+naumcheski@users.noreply.github.com>
2020-10-05 22:08:34 +0200

    Dodadena e nova linija kod

--- new-branch.js
+++ new-branch.js
@@ -1,3 +1,7 @@
 console.log("Suma od 5 i 6 e", 5 + 6);
 console.log("Nov branch e uspeshno kreiran");
 console.log("Kodiram vo master");
+console.log("Nov branch e uspeshno kreiran");
+console.log("Kodiram vo master");console.log("Nov branch e uspeshno kreiran");
+console.log("Kodiram vo master");
+console.log("new line");
